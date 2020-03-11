4b93e134577968ead1e8c4b1fc229ec62b60c944
sayonarasantos <sayyonarasantos@gmail.com>
2020-03-11 02:18:39 -0300

    Change in listHN and UnitTest

--- happyNumberUnitTest.py
+++ happyNumberUnitTest.py
@@ -6,23 +6,23 @@ class unitTestHappyNumber(unittest.TestCase):
     def setUp(self):
         self.number = HappyNumber()
 
-    def testIf1IsHappyNumber(self):
+    def testIf1IsAHappyNumber(self):
     	result = self.number.checkNumber(1)
     	self.assertEqual(result, 'Happy number')
 
-    def testIf7IsHappyNumber(self):
+    def testIf7IsAHappyNumber(self):
     	result = self.number.checkNumber(7)
     	self.assertEqual(result, 'Happy number')
 
-    def testIf130IsHappyNumber(self):
+    def testIf130IsAHappyNumber(self):
     	result = self.number.checkNumber(130)
     	self.assertEqual(result, 'Happy number')
 
-    def testIf2IsSadNumber(self):
+    def testIf2IsASadNumber(self):
     	result = self.number.checkNumber(2)
     	self.assertEqual(result, 'Sad number')
     
-    def testIf3IsSadNumber(self):
+    def testIf3IsASadNumber(self):
     	result = self.number.checkNumber(3)
     	self.assertEqual(result, 'Sad number')
 
--- listHN.py
+++ listHN.py
@@ -1,5 +1,5 @@
 from happyNumber import HappyNumber
 
 number = HappyNumber()
-for i in range(1,25):
+for i in range(1,10):
 	print(str(i)+" is a "+number.checkNumber(i))
